d67053616f9172228710cdbf6e3a0ce1cb9e6ebe
Kosmik <malpa.w.klapkach@gmail.com>
2024-06-26 19:58:03 +0200

    Create UIButton Events class

---
+++ Assets/Bipolar Core/UI/UIButtonEvents.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Bipolar.UI
+{
+	[RequireComponent(typeof(UIButton))]
+	public class UIButtonEvents : MonoBehaviour
+    {
+        private UIButton _button;
+        public UIButton Button => this.GetRequired(ref _button);
+
+        [SerializeField]
+        private UnityEvent onHover;
+        [SerializeField]
+        private UnityEvent onUnhover;
+
+		private void OnEnable()
+		{
+			Button.OnHighlightChanged += Button_OnHighlightChanged;
+		}
+
+		private void Button_OnHighlightChanged(UIButton button, bool highlighted)
+		{
+            var @event = highlighted ? onHover : onUnhover;
+            @event.Invoke();
+		}
+
+		private void OnDisable()
+		{
+			Button.OnHighlightChanged -= Button_OnHighlightChanged;
+		}
+	}
+}
