70a818743765e18b57f7c4b71a0ac8a4fb713b6c
Kosmik <malpa.w.klapkach@gmail.com>
2024-06-27 03:31:26 +0200

    Add bullet type class

---
+++ Assets/Scripts/BulletsCount.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class BulletsCount
+{
+	public BulletType bulletType;
+	public int count;
+
+	public BulletsCount(BulletType bulletType, int count)
+	{
+		this.bulletType = bulletType;
+		this.count = count;
+	}
+}
---
+++ Assets/Scripts/PlayerShootingController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+	[SerializeField]
+	private PlayerShooting playerShooting;
+
+	[SerializeField]
+	private List<BulletsCount> bulletsCounts;
+
+	[SerializeField]
+	private BulletType currentBulletType;
+	private BulletType CurrentBulletType => currentBulletType;
+
+	public void AddBullets(BulletType bulletType, int count)
+	{
+		int index = bulletsCounts.FindIndex(data => data.bulletType == bulletType);
+		if (index >= 0)
+			bulletsCounts[index].count += count;
+		else
+			bulletsCounts.Add(new BulletsCount(bulletType, count));
+	}
+
+	public int GetCount(BulletType bulletType)
+	{
+		int index = bulletsCounts.FindIndex(data => data.bulletType == bulletType);
+		return index < 0 ? 0 : bulletsCounts[index].count;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			playerShooting.Shoot(CurrentBulletType);
+		}
+	}
+
+}
